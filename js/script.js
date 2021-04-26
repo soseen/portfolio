613739cc5b27f2bfc5a3ac250d31233fd0838bd1
soseen <lampard082@gmail.com>
2021-04-26 02:22:48 +0200

    updated itil-events project link(deployed)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,5 @@
-// var blackBackground = false;
-
-
 $(window).scroll(function(){
     $('nav').toggleClass('black-bg', $(window).scrollTop() > 100);
 });
 
-// $('.fa-bars').click(function(){
-//     changeNavbarColor(blackBackground);
-// });
-
-// function changeNavbarColor(isBackgroundBlack){
-//     if(isBackgroundBlack === true){
-//         $('nav').css('background', "transparent");
-//         $('nav').css('border', "unset");
-//         blackBackground = false;
-//     }
-//     else {
-//         $('nav').css('background', "black");
-//         $('nav').css('border', "1px solid white");
-//         blackBackground = true;
-        
-//     }
-//     console.log(Boolean(blackBackground));
-//     return blackBackground;
-// }
 
